30267897a4d0eb95bffd15fffc3de2baf5f5b2e1
Dementev Daniil <ddaniild@list.ru>
2026-06-26 15:23:59 +0300

    Lint tests

--- interp-Lint.rkt
+++ interp-Lint.rkt
@@ -12,13 +12,7 @@
 		[(Prim 'read '()) #t]
 		[(Prim '- (list e1)) #f]
 		[(Prim '+ (list e1 e2)) #f]
-		[(Prim '- (list e1 e2)) #f]
-	)
-)
-
-; (displayln (leaf (Prim 'read '())))
-; (displayln (leaf (Int 8)))
-; (displayln (leaf (Prim '- (list (Int 8)))))
+		[(Prim '- (list e1 e2)) #f]))
 
 ; return #t if ast is Lint expression
 (define (is-exp ast)
@@ -28,26 +22,13 @@
 		[(Prim '- (list e)) (is-exp e)]
 		[(Prim '+ (list e1 e2)) (and (is-exp e1) (is-exp e2))]
 		[(Prim '- (list e1 e2)) (and (is-exp e1) (is-exp e2))]
-		[else #f]
-	)
-)
+		[else #f]))
 
 ; returns #t if ast is Lint program
 (define (is-Lint ast)
 	(match ast
 		[(Program '() e) (is-exp e)]
-		[else #f]
-	)
-)
-
-(define eight (Int 8))
-(define neg-eight (Prim '- (list eight)))
-(define rd (Prim 'read '()))
-(define ast1_1 (Prim '+ (list rd neg-eight)))
-
-; (displayln (is-exp rd))
-; (displayln (is-Lint (Program '() ast1_1)))
-; (displayln (is-Lint (Program '() (Prim '- (list (Prim 'read '()) (Prim '+ (list (Int 8) (Int 10))))))))
+		[else #f]))
 
 ; interpret expression
 (define (interp-exp e)
@@ -69,15 +50,75 @@
 		[(Prim '+ (list e1 e2))
 			(define v1 (interp-exp e1))
 			(define v2 (interp-exp e2))
-			(fx+ v1 v2)]
-	)
-)
+			(fx+ v1 v2)]))
 
 ; interpret program
 (define (interp-Lint p)
 	(match p
-		[(Program '() e) (interp-exp e)]
-	)
-)
+		[(Program '() e) (interp-exp e)]))
 
-; (displayln (interp-Lint (Program '() ast1_1)))
+; ---------------------------------------------------------------------------
+; Run with:  raco test interp-Lint.rkt
+(module+ test
+	(require rackunit 
+					 rackunit/text-ui)
+ 
+	; sample AST nodes
+	(define eight (Int 8))
+	(define neg-eight (Prim '- (list eight)))
+	(define rd (Prim 'read '()))
+	(define ast1_1 (Prim '+ (list rd neg-eight)))
+ 
+	(define leaf-tests
+	  (test-suite
+	   "leaf"
+	   (test-case "read is a leaf"
+	     (check-true (leaf rd)))
+	   (test-case "Int is a leaf"
+	     (check-true (leaf eight)))
+	   (test-case "unary minus is not a leaf"
+	     (check-false (leaf neg-eight)))
+	   (test-case "binary plus is not a leaf"
+	     (check-false (leaf (Prim '+ (list (Int 1) (Int 2))))))))
+ 
+	(define is-exp-tests
+	  (test-suite
+	   "is-exp"
+	   (test-case "lone read is a valid expression"
+	     (check-true (is-exp rd)))
+	   (test-case "nested arithmetic is a valid expression"
+	     (check-true (is-exp ast1_1)))
+	   (test-case "non-Lint operator is rejected"
+	     (check-false (is-exp (Prim '* (list (Int 2) (Int 3))))))))
+ 
+	(define is-Lint-tests
+	  (test-suite
+	   "is-Lint"
+	   (test-case "well-formed program"
+	     (check-true (is-Lint (Program '() ast1_1))))
+	   (test-case "program with binary minus"
+	     (check-true
+	      (is-Lint (Program '() (Prim '- (list (Prim 'read '())
+	                                            (Prim '+ (list (Int 8) (Int 10)))))))))
+	   (test-case "non-program is rejected"
+	     (check-false (is-Lint (Int 8))))))
+ 
+	; interpreter tests use read-free programs so raco test does not block on stdin
+	(define interp-tests
+	  (test-suite
+	   "interp-Lint"
+	   (test-case "single integer"
+	     (check-equal? (interp-Lint (Program '() (Int 8))) 8))
+	   (test-case "unary negation"
+	     (check-equal? (interp-Lint (Program '() neg-eight)) -8))
+	   (test-case "addition"
+	     (check-equal? (interp-Lint (Program '() (Prim '+ (list (Int 8) (Int 10))))) 18))
+	   (test-case "nested expression"
+	     (check-equal? (interp-Lint (Program '() (Prim '+ (list (Int 10)
+	                                                            (Prim '- (list (Prim '+ (list (Int 5) (Int 3)))))))))
+	                   2))))
+ 
+	(run-tests leaf-tests)
+	(run-tests is-exp-tests)
+	(run-tests is-Lint-tests)
+	(run-tests interp-tests))
